4073a37de3e3b25a8fda77af294c824392dd2ee4
odedsab <jsoded@campus.technion.ac.il>
2025-12-12 16:25:03 +0200

    Add basic GPIO LED control driver
    Adds a simple, dedicated driver for controlling an on-board LED.
    
    The driver handles the necessary low-level configuration:
    * Enables the clock for the GPIOA peripheral.
    * Configures pin PA5 for general-purpose output mode.
    * Exposes a clean interface (`led_init`, `led_on`, `led_off`, `led_toggle`) for easy control by application tasks.

--- drivers/led.c
+++ drivers/led.c
@@ -1,40 +1,33 @@
 #include "led.h"
-#include <stdint.h>
 
-#define PERIPH_BASE         0x40000000UL
-#define AHB2PERIPH_BASE     (PERIPH_BASE + 0x08000000UL)
-#define GPIOA_BASE          (AHB2PERIPH_BASE + 0x00UL)
-#define RCC_BASE            0x40021000UL
-#define RCC_AHB2ENR         (*(volatile uint32_t *)(RCC_BASE + 0x4C))
-#define GPIOA_MODER         (*(volatile uint32_t *)(GPIOA_BASE + 0x0))
-#define GPIOA_ODR           (*(volatile uint32_t *)(GPIOA_BASE + 0x14))
+#define RCC_AHB2ENR_GPIOAEN (1U << 0)
 
-#define LED_PIN_POS         5U
+#define LED_PIN_POS          5U
 #define LED_PIN_MASK        (1U << LED_PIN_POS)
 
 void led_init(void) {
-    /* Enable GPIOA clock - GPIOAEN */
-    RCC_AHB2ENR |= (1U << 0);
+    /* Enable GPIOA clock */
+    RCC->AHB2ENR |= RCC_AHB2ENR_GPIOAEN;
 
     /* 
         Set PA5 to output mode 01
         Moder has 2 bits for each pin
     */
-    GPIOA_MODER &= ~(0x3U << (LED_PIN_POS * 2)); // clear bits [11:10]
-    GPIOA_MODER |=  (0x1U << (LED_PIN_POS * 2)); // set bits [11:10] to 01
+    GPIOA->MODER &= ~(0x3U << (LED_PIN_POS * 2)); // clear bits [11:10]
+    GPIOA->MODER |=  (0x1U << (LED_PIN_POS * 2)); // set bits [11:10] to 01
 }
 
 void led_on(void) {
     /* set output to 1 */
-    GPIOA_ODR |= LED_PIN_MASK;
+    GPIOA->ODR |= LED_PIN_MASK;
 }
 
 void led_off(void) {
     /* set output to 0 */
-    GPIOA_ODR &= ~LED_PIN_MASK;
+    GPIOA->ODR &= ~LED_PIN_MASK;
 }
 
 void led_toggle(void) {
     /* toggle output bit */
-    GPIOA_ODR ^= LED_PIN_MASK;
+    GPIOA->ODR ^= LED_PIN_MASK;
 }
--- drivers/led.h
+++ drivers/led.h
@@ -1,6 +1,9 @@
 #ifndef LED_H
 #define LED_H
 
+#include <stdint.h>
+#include "device_registers.h"
+
 #ifdef __cplusplus
 extern "C" {
 #endif
